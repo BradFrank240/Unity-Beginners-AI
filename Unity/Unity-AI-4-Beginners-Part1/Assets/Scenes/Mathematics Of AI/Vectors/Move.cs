85ebd674ea7e0b4a61332567dfd282742f536a24
Pig <70529967+BradFrank240@users.noreply.github.com>
2021-03-29 09:52:58 -0400

    Hour in
    After an hour of work and eating breakfest.

---
+++ Unity/Unity-AI-4-Beginners-Part1/Assets/Scenes/Mathematics Of AI/Vectors/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    //Goal for bot to reach. 
+    //vector3 is the vector form of 3D
+    Vector3 Goal = new Vector3(6, 0, -4);
+
+
+    void Start()
+    {
+        Goal = Goal * 0.01f;
+          
+    }
+
+   
+    void Update()
+    {
+        this.transform.Translate(Goal);
+    }
+}
