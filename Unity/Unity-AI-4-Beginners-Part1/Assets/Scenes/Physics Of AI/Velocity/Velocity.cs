f2fe9284e3e5fb58d3a002b3b1f0015d86f55186
Pig <70529967+BradFrank240@users.noreply.github.com>
2021-03-30 09:27:43 -0400

    Started Physics
    Halfway through physics of AI. So far just simple concepts building on top of each other.

---
+++ Unity/Unity-AI-4-Beginners-Part1/Assets/Scenes/Physics Of AI/Velocity/Velocity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity : MonoBehaviour
+{
+
+    public float speed = 1f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.DrawRay(transform.position, transform.right * 10f, Color.blue, 5f);
+        }
+
+        //move forward by one meter per second
+        this.transform.Translate(Time.deltaTime * speed, Time.deltaTime, 0);
+        
+    }
+}
