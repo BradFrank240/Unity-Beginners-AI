31f66d885162428da14ebf78a8dd4c77733d0847
Pig <70529967+BradFrank240@users.noreply.github.com>
2021-04-03 13:35:25 -0400

    Finished Graphs & Waypoints
    gg no re

--- Unity/Unity-AI-4-Beginners-Part2/Assets/WPManager.cs
+++ Unity/Unity-AI-4-Beginners-Part2/Assets/WPManager.cs
@@ -17,14 +17,37 @@ public class WPManager : MonoBehaviour
 {
 
     public GameObject[] waypoints;
-    public Link[] linkes;
+    public Link[] links;
     public Graph graph = new Graph();
 
 
 
 
+    private void Start()
+    {
+        if(waypoints.Length > 0)
+        {
+            foreach(GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+
+            foreach(Link l in links)
+            {
+                graph.AddEdge(l.node1, l.node2);
+                if(l.dir == Link.direction.BI)
+                {
+                    graph.AddEdge(l.node2, l.node1);
+                }
+            }
+        }
+    }
 
 
+    private void Update()
+    {
+        graph.debugDraw();
+    }
 
 
 
