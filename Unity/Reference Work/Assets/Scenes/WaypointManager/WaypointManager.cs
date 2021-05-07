6c9c5ae1f08083aee1a9e51d3b4ef2dae56c47bd
Pig <jengalabscp@gmail.com>
2021-05-07 07:31:16 -0400

    work

---
+++ Unity/Reference Work/Assets/Scenes/WaypointManager/WaypointManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//Singletone for manageing the waypoints on a map that can be accessed by all scripts
+public class WaypointManager 
+{
+
+    
+
+
+
+} 
