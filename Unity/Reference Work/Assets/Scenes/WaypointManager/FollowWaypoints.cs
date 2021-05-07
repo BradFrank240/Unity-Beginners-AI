0b23f7c248e658e7ce3e69da3b28e85560351f7e
Jenkins <jengalabscp@gmail.com>
2021-05-07 09:45:06 -0400

    Working Singletons

---
+++ Unity/Reference Work/Assets/Scenes/WaypointManager/FollowWaypoints.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWaypoints : MonoBehaviour
+{
+    
+    //Store waypoints
+    List<GameObject> waypoints = new List<GameObject>();
+
+    private GameObject _Waypoint;
+
+    private int _CurrentWaypoint = 0;
+
+    public float accuracy = 2f;
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 50f;
+
+
+    private void Start()
+    {
+        waypoints.AddRange(WaypointManager.waypointManager.waypoints);
+        //set first waypoint
+        _Waypoint = waypoints[_CurrentWaypoint];
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        //check if close enough to waypoint
+        if (Vector3.Distance(this.transform.position, _Waypoint.transform.position) > 2f)
+        {
+            //find the roation value to look at goal
+            Quaternion lookAtGoal = Quaternion.LookRotation(_Waypoint.transform.position - this.transform.position);
+            
+            //Look at goal
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookAtGoal, rotationSpeed * Time.deltaTime);
+
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        }
+        else
+        {
+            //check if next goal exceeds limit
+            _CurrentWaypoint++;
+            if (_CurrentWaypoint > waypoints.Count)
+            {
+                _CurrentWaypoint = 0;
+            }
+            
+            _Waypoint = waypoints[_CurrentWaypoint];
+        }
+
+        
+    }
+
+
+
+
+}
--- Unity/Reference Work/Assets/Scenes/WaypointManager/WaypointManager.cs
+++ Unity/Reference Work/Assets/Scenes/WaypointManager/WaypointManager.cs
@@ -8,7 +8,43 @@ using UnityEngine;
 public class WaypointManager 
 {
 
+    //Hold the singoleton
+    private static WaypointManager _instance;
+
+    //keep a list of waypoints
+    private List<GameObject> _waypoints = new List<GameObject>();
+
+    //a list of waypoint with public access
+    public List<GameObject> waypoints { get { return _waypoints;  } }
+
+
+
+
     
+    //Create and and set properties of singleton
+    public static WaypointManager waypointManager
+    {
+        get
+        {
+            //check singleton exist
+            if(_instance == null)
+            {
+                //create new manager
+                _instance = new WaypointManager();
+                //set properties
+                _instance._waypoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+
+
+            }
+
+            //return self
+            return _instance;
+        }
+
+    }
+   
+
+
 
 
 
